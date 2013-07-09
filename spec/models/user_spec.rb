7ba6bc5dd4b3a7463ef041b2852deb821e259315
lalalainexd <lalalainexd@gmail.com>
2013-07-09 11:59:54 -0600

    Add spec for feed_items with param

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,22 +12,47 @@ describe User do
 
   describe "#feed_items" do
 
-    it "gets all the statuses" do
-      u = User.create_with_password("test", "123", "123")
-      source1 = User.create_with_password("source1", "123", "123")
-      source2 = User.create_with_password("source2", "123", "123")
-      u.add_feed_source(source1)
-      u.add_feed_source(source2)
+    context "with default param" do
+      it "gets all the statuses" do
+        u = User.create_with_password("test", "123", "123")
+        source1 = User.create_with_password("source1", "123", "123")
+        source2 = User.create_with_password("source2", "123", "123")
+        u.add_feed_source(source1)
+        u.add_feed_source(source2)
 
-      status1 = source1.statuses.create(text: "hello")
-      status2 = source1.statuses.create(text: "hello2")
-      status3 = source2.statuses.create(text: "hello3")
-      status4 = source2.statuses.create(text: "hello4")
+        status1 = source1.statuses.create(text: "hello")
+        status2 = source1.statuses.create(text: "hello2")
+        status3 = source2.statuses.create(text: "hello3")
+        status4 = source2.statuses.create(text: "hello4")
 
-      expect(u.feed_items).to include(status1)
-      expect(u.feed_items).to include(status2)
-      expect(u.feed_items).to include(status3)
-      expect(u.feed_items).to include(status4)
+        expect(u.feed_items).to include(status1)
+        expect(u.feed_items).to include(status2)
+        expect(u.feed_items).to include(status3)
+        expect(u.feed_items).to include(status4)
+      end
+    end
+
+    context "with a time" do
+
+      it "gets all statuses after the time" do
+        u = User.create_with_password("test", "123", "123")
+        source1 = User.create_with_password("source1", "123", "123")
+        source2 = User.create_with_password("source2", "123", "123")
+        u.add_feed_source(source1)
+        u.add_feed_source(source2)
+
+        status1 = source1.statuses.create(text: "hello")
+        status2 = source1.statuses.create(text: "hello2")
+        sleep(2);
+        status3 = source2.statuses.create(text: "hello3")
+        status4 = source2.statuses.create(text: "hello4")
+
+        feed_items = u.feed_items(status2.created_at)
+        expect(feed_items).to_not include(status1)
+        expect(feed_items).to_not include(status2)
+        expect(feed_items).to include(status3)
+        expect(feed_items).to include(status4)
+      end
     end
 
   end
